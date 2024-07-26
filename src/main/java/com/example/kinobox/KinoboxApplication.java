9be051bdae1bf47951ed36d06b50fcaa1639317c
Lale Mammadli <yavuzadpu@gmail.com>
2024-07-26 23:20:30 +0400

    first commit

---
+++ src/main/java/com/example/kinobox/KinoboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinobox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kinobox/KinoboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinobox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
